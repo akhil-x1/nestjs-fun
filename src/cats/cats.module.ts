040150699120a05ec21a83102cd1521daf62c01e
akhil <akhil@geekyants.com>
2026-08-20 12:47:40 +0530

    Added export comment to CatModule.

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -6,5 +6,6 @@ import { CatService } from './cat.service';
 @Module({
   controllers: [CatController],
   providers: [CatService],
+  exports: [CatService] //Exported provider can be imported by some other modules. This prevents creating a separate instance of the service causing increased memory, compute and other resource usage.
 })
 export class CatModule {}
